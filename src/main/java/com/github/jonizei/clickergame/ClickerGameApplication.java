677099d94b4855169439ccf5b091ffb3228d24d8
jonizei <joni.koskinen@cs.tamk.fi>
2020-02-13 15:36:16 +0200

    Initial commit

---
+++ src/main/java/com/github/jonizei/clickergame/ClickerGameApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonizei.clickergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickerGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jonizei/clickergame/ClickergameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonizei.clickergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickergameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
